86016ae8b56aab25ad3a49a973baaab8ce3aff89
Jorge Baez <jibm_82@yahoo.com.mx>
2018-05-04 22:42:07 -0500

    Integrated friendly routes for blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,13 +2,11 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   # GET /blogs
-  # GET /blogs.json
   def index
     @blogs = Blog.all
   end
 
   # GET /blogs/1
-  # GET /blogs/1.json
   def show
   end
 
@@ -22,7 +20,6 @@ class BlogsController < ApplicationController
   end
 
   # POST /blogs
-  # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
 
@@ -36,7 +33,6 @@ class BlogsController < ApplicationController
   end
 
   # PATCH/PUT /blogs/1
-  # PATCH/PUT /blogs/1.json
   def update
     respond_to do |format|
       if @blog.update(blog_params)
